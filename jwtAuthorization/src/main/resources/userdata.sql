33fff5bbe2d2d9d60ae57661a0e95788e1dc150a
aritrog <aritraghosh084@gmail.com>
2021-07-05 00:58:22 +0530

    jwt done

---
+++ jwtAuthorization/src/main/resources/userdata.sql
@@ -0,0 +1,5 @@
+CREATE TABLE userdata(user_id VARCHAR(255), user_password VARCHAR(255), user_name VARCHAR(255));
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('1','Aritra','12345');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('2','Keshore','12345');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('3','Sonu','12345');
+INSERT INTO userdata (user_id,user_name,user_password) VALUES ('4','Jeenath','12345');
