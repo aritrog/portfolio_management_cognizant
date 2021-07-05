ab6fd382dbd114c6eaf7bc764031b46fb69873c3
Keshore12 <43296396+Keshore12@users.noreply.github.com>
2021-07-05 13:34:45 +0530

    keshore-completed

---
+++ calculateNetworth/target/classes/import.sql
@@ -0,0 +1,9 @@
+insert into asset values ('asset1','JIO','share',5,'101');
+insert into asset values ('asset2','HDFC','mutual',10,'101');
+
+
+insert into asset values ('asset3','HP','share',10,'102');
+insert into asset values ('asset4','SBI','mutual',15,'102');
+
+insert into asset values ('asset5','JIO','share',5,'101');
+insert into asset values ('asset6','HP','share',10,'101');
