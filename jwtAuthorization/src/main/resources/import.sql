82be28c2cf35b1d408b3df1fbabc91d4ad07fa3a
Keshore12 <43296396+Keshore12@users.noreply.github.com>
2021-07-13 19:22:25 +0530

    update

--- jwtAuthorization/src/main/resources/import.sql
+++ jwtAuthorization/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-CREATE TABLE userdata(user_id VARCHAR(255), user_password VARCHAR(255), user_name VARCHAR(255));
+-- CREATE TABLE userdata(user_id VARCHAR(255), user_password VARCHAR(255), user_name VARCHAR(255));
 INSERT INTO userdata (user_id,user_name,user_password) VALUES ('1','Aritra','12345');
 INSERT INTO userdata (user_id,user_name,user_password) VALUES ('2','Keshore','12345');
 INSERT INTO userdata (user_id,user_name,user_password) VALUES ('3','Sonu','12345');
